bccde4fe4327494564bd25500bc0cb24487fc207
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-01-18 11:48:23 +0300

    feat: add name and artist_names vars to Track

--- src/api/track.py
+++ src/api/track.py
@@ -1,13 +1,20 @@
 class Track:
-    def __init__(self, id: str, album_image_url: str):
+    def __init__(self, id: str, name: str, artist_names: list[str], album_image_url: str):
         self.id = id
+        self.name = name
+        self.artist_names = artist_names
         self.album_image_url = album_image_url
 
     @classmethod
     def from_api_data(cls, track_data):
         """Create a Track object from Spotify API data."""
-        id = track_data.get('id')
+        id = track_data.get('id', '')
+        name = track_data.get('name', '')
+
+        artists = track_data.get('artists', [])
+        artist_names = [artist.get('name') for artist in artists if artist.get('name') is not None]
+
         album_images = track_data.get('album', {}).get('images')
-        if album_images is not None:
-            album_image_url = album_images[0].get('url')
-            return cls(id=id, album_image_url=album_image_url)
+        album_image_url = album_images[0].get('url') if album_images is not None else ''
+
+        return cls(id=id, name=name, artist_names= artist_names, album_image_url=album_image_url)
