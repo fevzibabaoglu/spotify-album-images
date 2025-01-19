91e4e48c3b5099e55eee755d4a9dd7dd777e2b7a
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-01-19 21:15:07 +0300

    refactor: put constants at top

--- src/main.py
+++ src/main.py
@@ -6,8 +6,18 @@ from image_ops import ImageHandler, TrackImage, Clustering
 
 
 def main(playlist_id, max_workers):
+# Default args
+MAX_WORKERS_DEFAULT = 10
+
+# Constants
+OUTPUT_DIRNAME = 'output'
+ALBUM_IMAGE_SHAPE = (256, 256)
+CLUSTER_IMAGE_SHAPE = (640, 640)
+CLUSTER_IMAGE_FILENAME = 'clustered_image.jpg'
+
+
     # Setup output directory
-    output_dir = os.path.join(os.getcwd(), 'output')
+    output_dir = os.path.join(os.getcwd(), OUTPUT_DIRNAME)
 
     # Initialize Spotify API and authenticate
     spotifyAPI = SpotifyAPI()
@@ -21,7 +31,7 @@ def main(playlist_id, max_workers):
     playlist = Playlist(spotifyAPI, playlist_id)
 
     # Download and process the images
-    images = track_image.handle_images(playlist, output_shape=(256, 256))
+    images = track_image.handle_images(playlist, output_shape=ALBUM_IMAGE_SHAPE)
 
     # Initialize and apply clustering
     n_clusters = 5  # Adjust as needed
@@ -36,12 +46,12 @@ def main(playlist_id, max_workers):
     clustering.fit(images)
 
     # Get cluster image in RGB space
-    cluster_image = clustering.get_cluster_image(output_shape=(640, 640))
+    cluster_image = clustering.get_cluster_image(output_shape=CLUSTER_IMAGE_SHAPE)
 
     # Save cluster image in RGB space
     if track_image.save_album_images:
         playlist_folder = image_handler.create_folder(playlist.playlist_id)
-        filename = image_handler.sanitize_filename('clustered_image.jpg')
+        filename = image_handler.sanitize_filename(CLUSTER_IMAGE_FILENAME)
         save_path = os.path.join(playlist_folder, filename)
         cluster_image.save(save_path)
         
@@ -53,7 +63,7 @@ if __name__ == '__main__':
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Cluster album images from a Spotify playlist.")
     parser.add_argument('playlist_id', type=str, help="The ID of the Spotify playlist.")
-    parser.add_argument('--max_workers', type=int, default=10, help="The number of workers for parallel processing.")
+    parser.add_argument('--max_workers', type=int, default=MAX_WORKERS_DEFAULT, help="The number of workers for parallel processing.")
     args = parser.parse_args()
 
     # Run main with parsed arguments
